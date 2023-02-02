1bc90d8a97155cad6acf2a9e6db8e0f13e3931f2
olku11 <artem.chastukhin@gmail.com>
2023-02-02 09:46:19 +0300

    first1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
@@ -34,7 +35,7 @@ def return_sample_page():
 
 @app.route('/image_mars')
 def mars_im():
-    return """<!doctype html>
+    return f"""<!doctype html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
@@ -43,6 +44,41 @@ def mars_im():
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                    
+                  </body>"""
+
+
+@app.route('/promotion_image')
+def prom_im():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                     <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-dark" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
                     
                   </body>"""
 
